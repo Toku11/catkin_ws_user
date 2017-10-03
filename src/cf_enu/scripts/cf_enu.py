d631728a164e03faa3c2a0370e6b50c128463c24
Toku <fa_ing.toku@hotmail.com>
2017-10-02 19:48:59 -0500

    cf_enu terminado

--- src/cf_enu/scripts/cf_enu.py
+++ src/cf_enu/scripts/cf_enu.py
@@ -9,6 +9,7 @@ Created on Tue Sep 13 12:02:53 2016
 from __future__ import print_function
 import sys
 import rospy
+import numpy as np
 import roslib
 import tf
 roslib.load_manifest('cf_enu')
@@ -18,10 +19,34 @@ from std_msgs.msg import Float32
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, Quaternion, Vector3
 
-def conversion(ini,final):
-	ne,nn,nu=[1,2,3]
+def conversion(llh):
+	llh0=np.array([-99.12718,  19.47771,   0.     ])
+	a = 6378137
+	b = 6356752.3142
+	e2 = 1 - (b/a)**2
+	#%%%%%%%%%%Location of reference point in radians
+	phi = llh0[1]*np.pi/180;
+	lam = llh0[0]*np.pi/180;
+	h = llh0[2]
 
-	return ne,nn,nu
+	#%%%%%%%%%%Location of data points in radians
+	dphi= llh[:,1]*np.pi/180 - phi;
+	dlam= llh[:,0]*np.pi/180 - lam;
+	dh = llh[:,2] - h;
+	#%%%%%%%%%%Some useful definitions
+	tmp1 = np.sqrt(1-e2*np.sin(phi)**2);
+	cl = np.cos(lam);
+	sl = np.sin(lam);
+	cp = np.cos(phi);
+	sp = np.sin(phi);
+	#%%%%%%%%%%Transformations
+	de = (a/tmp1+h)*cp*dlam - (a*(1-e2)/(tmp1**3)+h)*sp*dphi*dlam +cp*dlam*dh
+	dn = (a*(1-e2)/tmp1**3 + h)*dphi + 1.5*cp*sp*a*e2*dphi**2 + sp**2*dh*dphi + 0.5*sp*cp*(a/tmp1 +h)*dlam**2
+	du = dh - 0.5*(a-1.5*a*e2*cp**2+0.5*a*e2+h)*dphi**2 - 0.5*cp**2*(a/tmp1 -h)*dlam**2
+
+	denu = np.array([de, dn, du])
+
+	return denu
 
 class cf_enu:
 
@@ -39,13 +64,23 @@ class cf_enu:
 		self.msg.pose.pose.orientation=Quaternion(*q)
 		if (self.orientation_received==False):
 			self.orientation_received=True
-
+		if orientation_received and gps_received:
+			self.gps_odometry_pub.publish(self.msg)
+			
 	def conversion_callback(self, gps_coords):
 		self.msg.header.stamp=rospy.Time.now()
 		self.msg.header.frame_id=self.frame_id
 		self.msg.child_fram_id=self.child_frame_id
-		self.msg.pose.pose.position=Point(e,n,u)
-		self.gps_odometry_pub.publish(self.msg)
+		latitud=gps_coords.latitud()
+		longitud=gps_coords.longitud()
+		altitud=gps.coords.altitud()
+		coords=np.array([longitud,latitud,altitud])
+		enu=conversion(coords)
+		self.msg.pose.pose.position=Point(e[0],n[1],u[2])
+		if self.gps_received==False:
+			self.gps_received=True
+		if orientation_received and gps_received:
+			self.gps_odometry_pub.publish(self.msg)
 		
 
 
